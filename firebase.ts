ee8c609601f2c7c1696bffdf6baaa5bad3b195bd
Galuxier <galucier2004@gmail.com>
2025-03-31 00:03:34 +0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCYAVtqtxU2xKuP7-DYSFa0o9E2lL0Lw2M",
-    authDomain: "chat-app-5c9d2.firebaseapp.com",
-    projectId: "chat-app-5c9d2",
-    storageBucket: "chat-app-5c9d2.appspot.com",
-    messagingSenderId: "1071862760597",
-    appId: "1:1071862760597:web:1b3a8d3c7cc6e1c6e6479c",
-    measurementId: "G-LLVZMV60N4"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase app if not already initialized
